7147b2a84097dd3b68cfd5b4d4ed9a9f0f763b14
ReneVajda1 <rene.vajda@yahoo.com>
2025-01-08 03:52:40 +0100

    Inventory class + internal logic implemented

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,3 +1,28 @@
+import java.util.ArrayList;
+
 public class Inventory {
+
+    // 1 declaring item Objects
+    private ArrayList<Item> items;
+
+// generated constructor
+//    public Inventory(ArrayList<Item> items) {
+//        this.items = items;
+//    }
+
+    // modified constructor with new ArrayList of items
+public Inventory(){
+    items = new ArrayList<>();
+}
+
+public void addItem(Item item){
+    items.add(item);
+}
+
+public void displayInventory(){
+    for (Item item: items){
+        System.out.println("Item: " + item.getName() + ", Quantity: " + item.getQuantity());
+    }
+}
 
 }
